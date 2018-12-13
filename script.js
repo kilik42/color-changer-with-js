59ccde67b8027626469227dac19bf16709404ec2
marvin evins <maevins@cps.edu>
2018-12-12 18:49:07 -0600

    updated script

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ const btn = document.getElementById('btn');
 
 btn.addEventListener('click', function(){
   let random = Math.floor(Math.random()*colors.length);
+
+  const body = document.body;
+  body.style.backgroundColor = colors[random];
+  
   console.log(random);
 
 
